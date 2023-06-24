4afe16b54f874b9f45cea2128a3f9fd48620aa62
Hokin <jian.hee@qq.com>
2023-06-24 23:34:41 +0800

    commit

--- chapter04/4-2.c
+++ chapter04/4-2.c
@@ -4,5 +4,14 @@
 
 int main()
 {
+    double score = 0;
+    int i = 0;
+    double sum = 0;
+    for (i = 0; i < 3; i++)
+    {
+        scanf("%lf", &score);
+        sum += score;
+    }
+    printf("%.2lf %.2lf", sum, sum / 3.0);
     return 0;
 }
