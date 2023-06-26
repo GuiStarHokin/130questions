56d0650897d6962b00489d24f05fdfa9a4dcf8c8
Hokin <jian.hee@qq.com>
2023-06-26 23:14:29 +0800

    commit

--- chapter04/4-2.c
+++ chapter04/4-2.c
@@ -1,17 +1,17 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-
-int main()
-{
-    double score = 0;
-    int i = 0;
-    double sum = 0;
-    for (i = 0; i < 3; i++)
-    {
-        scanf("%lf", &score);
-        sum += score;
-    }
-    printf("%.2lf %.2lf", sum, sum / 3.0);
-    return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    double score = 0;
+//    int i = 0;
+//    double sum = 0;
+//    for (i = 0; i < 3; i++)
+//    {
+//        scanf("%lf", &score);
+//        sum += score;
+//    }
+//    printf("%.2lf %.2lf", sum, sum / 3.0);
+//    return 0;
+//}
--- chapter04/4-3.c
+++ chapter04/4-3.c
@@ -1,22 +1,22 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-
-int main()
-{
-    int n = 0;
-    int h = 0;
-    int m = 0;
-    while ((scanf("%d %d %d", &n, &h, &m)) != EOF)
-    {
-        if (m % h)
-        {
-            printf("%d", n - (m / h) - 1);
-        }
-        else
-        {
-            printf("%d", n - (m / h));
-        }
-    }
-    return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int n = 0;
+//    int h = 0;
+//    int m = 0;
+//    while ((scanf("%d %d %d", &n, &h, &m)) != EOF)
+//    {
+//        if (m % h)
+//        {
+//            printf("%d", n - (m / h) - 1);
+//        }
+//        else
+//        {
+//            printf("%d", n - (m / h));
+//        }
+//    }
+//    return 0;
+//}
