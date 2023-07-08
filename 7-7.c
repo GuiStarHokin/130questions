9bcbcd8eef12873462b1ea1b81beba861fef206e
hokin <jian.hee@qq.com>
2023-07-08 20:40:24 +0800

    commit

---
+++ 7-7.c
@@ -0,0 +1,60 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    int n = 0;
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        int i = 0;
+//        for (i=0; i<n; i++)
+//        {
+//            int j = 0;
+//            for (j=0; j<i; j++)
+//            {
+//                printf(" ");
+//            }
+//            for (j=0; j<1; j++)
+//            {
+//                printf("*");
+//            }
+//            for (j=0; j<n-1-i; j++)
+//            {
+//                printf(" ");
+//            }
+//            printf("\n");
+//
+//        }
+//
+//    }
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    int n = 0;
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        int i = 0;
+//        for (i = 0; i < n; i++)
+//        {
+//            int j = 0;
+//            for (j = 0; j < n; j++)
+//            {
+//                if (i == j)
+//                {
+//                    printf("*");
+//                }
+//                else
+//                {
+//                    printf(" ");
+//                }
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
