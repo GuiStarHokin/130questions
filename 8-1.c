ebbf991eab963dcbf0f2c701a25e50a09b6b8f47
hokin <jian.hee@qq.com>
2023-07-10 20:10:17 +0800

    commit

---
+++ 8-1.c
@@ -0,0 +1,36 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    double arr[5] = {0.0};
+//    int i = 0;
+//    double sum = 0.0;
+//    for (i=0; i<5; i++)
+//    {
+//        scanf("%lf", &arr[i]);
+//    }
+//    for (i=0; i<5; i++)
+//    {
+//        sum += arr[i];
+//    }
+//    printf("%.2lf\n", sum/5.0);
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    double hight = 0.0;
+//    double sum = 0.0;
+//    int i = 0;
+//    for (i = 0; i < 5; i++)
+//    {
+//        scanf("%lf", &hight);
+//        sum += hight;
+//    }
+//    printf("%.2lf", sum / 5);
+//    return 0;
+//}
