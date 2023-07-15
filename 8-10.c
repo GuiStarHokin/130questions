87a075d930dd53deaf8fa941d66b85565d4c3319
hokin <jian.hee@qq.com>
2023-07-15 19:49:09 +0800

    commit

---
+++ 8-10.c
@@ -0,0 +1,42 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    int n = 0;
+//    int odd = 0;
+//    int even = 0;
+//    int i = 0;
+//    scanf("%d", &n);
+//    for (i=1; i<=n; i++)
+//    {
+//        if (i % 2 == 0)
+//        {
+//            even++;
+//        }
+//        else
+//        {
+//            odd++;
+//        }
+//    }
+//    printf("%d %d\n", odd, even);
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    if (n % 2 == 0)
+//    {
+//        printf("%d %d\n", n / 2, n / 2);
+//    }
+//    else
+//    {
+//        printf("%d %d\n", (n / 2) + 1, n / 2);
+//    }
+//    return 0;
+//}
--- 8-9.c
+++ 8-9.c
@@ -1,2 +1,54 @@
-#define _CRT_SECURE_NO_WARNINGS
-
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    int i = 0;
+//    int count = 0;
+//    for (i=1; i<=2019; i++)
+//    {
+//        // 1999
+//        // 个位
+//        if (i % 10 == 9) // 9
+//        {
+//            count++;
+//        }
+//        // 十位
+//        else if ((i / 10) % 10 == 9) // 199 % 10
+//        {
+//            count++;
+//        }
+//        // 百位
+//        else if ((i / 100) % 10 == 9)
+//        {
+//            count++;
+//        }
+//    }
+//
+//    printf("%d\n", count);
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    int count = 0;
+//    int i = 0;
+//    for (i = 1; i <= 2019; i++)
+//    {
+//        int m = i;
+//        while (m)
+//        {
+//            if (m % 10 == 9)
+//            {
+//                count++;
+//                break;
+//            }
+//            m /= 10;
+//        }
+//    }
+//    printf("%d\n", count);
+//    return 0;
+//}
