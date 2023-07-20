740eacb21a6b6873a919063d8ab9753372db2b47
hokin <jian.hee@qq.com>
2023-07-20 19:36:30 +0800

    commit

---
+++ 9-7.c
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int i = 0;
+//    int max = 0;
+//    int min = 100;
+//    int num = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d", &num);
+//        if (num >= max)
+//        {
+//            max = num;
+//        }
+//        if (num <= min)
+//        {
+//            min = num;
+//        }
+//    }
+//    printf("%d\n", max - min);
+//    return 0;
+//}
