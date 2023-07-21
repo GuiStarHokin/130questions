8bd8022a5b1d128f8b0461a3f38495f4738e405d
hokin <jian.hee@qq.com>
2023-07-21 20:37:58 +0800

    commit

---
+++ 9-8.c
@@ -0,0 +1,153 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int i = 0;
+//    int count1 = 0;
+//    int count2 = 0;
+//    int arr[n];
+//    for (i=0; i<n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    for (i=0; i<n-1; i++)
+//    {
+//        if (arr[i] >= arr[i+1])
+//        {
+//            count1++;
+//        }
+//        else if (arr[i] <= arr[i+1])
+//        {
+//            count2++;
+//        }
+//    }
+//    if (count1 == n-1 || count2 == n-1)
+//    {
+//        printf("sorted\n");
+//    }
+//    else
+//    {
+//        printf("unsorted\n");
+//    }
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int i = 0;
+//    int arr[n];
+//    int flag1 = 0;
+//    int flag2 = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    for (i = 0; i < n - 1; i++)
+//    {
+//        if (1)
+//        {
+//            if (arr[i] >= arr[i - 1])
+//            {
+//                flag1 = 1;
+//            }
+//            else
+//            {
+//                flag2 = 1;
+//            }
+//        }
+//    }
+//    if (flag1 + flag2 == 1)
+//    {
+//        printf("sorted\n");
+//    }
+//    else
+//    {
+//        printf("unsorted\n");
+//    }
+//
+//    return 0;
+//}#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int i = 0;
+//    int count1 = 0;
+//    int count2 = 0;
+//    int arr[n];
+//    for (i=0; i<n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    for (i=0; i<n-1; i++)
+//    {
+//        if (arr[i] >= arr[i+1])
+//        {
+//            count1++;
+//        }
+//        else if (arr[i] <= arr[i+1])
+//        {
+//            count2++;
+//        }
+//    }
+//    if (count1 == n-1 || count2 == n-1)
+//    {
+//        printf("sorted\n");
+//    }
+//    else
+//    {
+//        printf("unsorted\n");
+//    }
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int i = 0;
+//    int arr[n];
+//    int flag1 = 0;
+//    int flag2 = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    for (i = 0; i < n - 1; i++)
+//    {
+//        if (1)
+//        {
+//            if (arr[i] >= arr[i - 1])
+//            {
+//                flag1 = 1;
+//            }
+//            else
+//            {
+//                flag2 = 1;
+//            }
+//        }
+//    }
+//    if (flag1 + flag2 == 1)
+//    {
+//        printf("sorted\n");
+//    }
+//    else
+//    {
+//        printf("unsorted\n");
+//    }
+//
+//    return 0;
+//}
