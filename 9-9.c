f374601af92283ac5c55df62ecd6a4de54e15ed9
hokin <jian.hee@qq.com>
2023-07-22 21:04:05 +0800

    commit

--- 9-9.c
+++ 9-9.c
@@ -1,2 +1,31 @@
-#define _CRT_SECURE_NO_WARNINGS
-
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int arr[50] = { 0 };
+//    int i = 0;
+//    int num = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    scanf("%d", &num);
+//    int j = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        if (arr[i] != num)
+//        {
+//            arr[j] = arr[i];
+//            j++;
+//        }
+//    }
+//    for (i = 0; i < j; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
