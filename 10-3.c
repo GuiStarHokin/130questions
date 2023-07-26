bd4c4b0a2b9f55e554ac126171d1fae9464bd7c6
hokin <jian.hee@qq.com>
2023-07-26 20:47:14 +0800

    commit

---
+++ 10-3.c
@@ -0,0 +1,73 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    int n = 0;
+//    int m = 0;
+//    scanf("%d %d", &n, &m);
+//    int i = 0;
+//    int arr[n][m];
+//    for (i=0; i<n; i++)
+//    {
+//        int j = 0;
+//        for (j=0; j<m; j++)
+//        {
+//            scanf("%d ", &arr[i][j]);
+//        }
+//    }
+//    for (i=0; i<n; i++)
+//    {
+//        int j = 0;
+//        for (j=0; j<m; j++)
+//        {
+//            printf("%d ", arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//*/
+//
+///*
+//int main()
+//{
+//    int n = 0;
+//    int m = 0;
+//    scanf("%d %d", &n, &m);
+//    int i = 0;
+//    int num = 0;
+//    for (i=0; i<n; i++)
+//    {
+//        int j = 0;
+//        for (j=0; j<m; j++)
+//        {
+//            scanf("%d", &num);
+//            printf("%d ", num);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    int n = 0;
+//    int m = 0;
+//    scanf("%d %d", &n, &m);
+//    int i = 0;
+//    int num = 0;
+//    for (i = 1; i <= n * m; i++)
+//    {
+//        scanf("%d ", &num);
+//        printf("%d ", num);
+//        if ((i % m) == 0)
+//        {
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
---
+++ 10-4.c
@@ -0,0 +1,61 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    int n = 0;
+//    int m = 0;
+//    scanf("%d %d", &n, &m);
+//    int arr[n][m];
+//    int i = 0;
+//    int x = 0;
+//    int y = 0;
+//    int max = 0;
+//    for (i=0; i<n; i++)
+//    {
+//        int j = 0;
+//        for (j=0; j<m; j++)
+//        {
+//            scanf("%d", &arr[i][j]);
+//            if (arr[i][j] >= max)
+//            {
+//                max = arr[i][j];
+//                x = i;
+//                y = j;
+//            }
+//        }
+//    }
+//    printf("%d %d\n", x+1, y+1);
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    int n = 0;
+//    int m = 0;
+//    scanf("%d %d", &n, &m);
+//    int max = 0;
+//    int i = 0;
+//    int num = 0;
+//    int x = 0;
+//    int y = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < m; j++)
+//        {
+//            scanf("%d ", &num);
+//            if (num > max)
+//            {
+//                max = num;
+//                x = i;
+//                y = j;
+//            }
+//        }
+//    }
+//    printf("%d %d\n", x + 1, y + 1);
+//    return 0;
+//}
