c871ae624d3cd241ba8e8132c87c6c18c526ba21
hokin <jian.hee@qq.com>
2023-08-02 10:14:16 +0800

    commit

---
+++ 11-1.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void Senary(int n)
+{
+    if (n > 0)
+    {
+        int a = n % 6;
+        n /= 6;
+        Senary(n);
+        printf("%d", a);
+
+    }
+}
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    Senary(n);
+    return 0;
+}
