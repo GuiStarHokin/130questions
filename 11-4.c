44da586f45b6d53b9c68fec8b8fb77f599de06dd
hokin <jian.hee@qq.com>
2023-08-07 20:23:06 +0800

    commit

---
+++ 11-4.c
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int times = n / 12; // 12个的次数
+//    int t = 0;
+//    while (times--)
+//    {
+//        t += 4;
+//    }
+//    t += 2;
+//    printf("%d\n", t);
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    printf("%d\n", (n / 12) * 4 + 2);
+//    return 0;
+//}
