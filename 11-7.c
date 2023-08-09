629514a77b574f6d8b34273e954fd8dcdef1cdb7
hokin <jian.hee@qq.com>
2023-08-09 20:41:52 +0800

    commit

---
+++ 11-7.c
@@ -0,0 +1,31 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int f(int n)
+//{
+//    if (n == 0)
+//    {
+//        return 0;
+//    }
+//    else if (n == 1)
+//    {
+//        return 1;
+//    }
+//    else if (n == 2)
+//    {
+//        return 2;
+//    }
+//    else
+//    {
+//        return (f(n - 1) + f(n - 2));
+//    }
+//}
+//
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int ret = f(n);
+//    printf("%d\n", ret);
+//    return 0;
+//}
