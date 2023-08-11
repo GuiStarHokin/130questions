558685b5d055c70bdd0717a1c1f5e38bc73479ba
hokin <jian.hee@qq.com>
2023-08-11 20:37:08 +0800

    commit

---
+++ 11-11.c
@@ -0,0 +1,70 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+///*
+//int main()
+//{
+//    int score = 0;
+//    scanf("%d", &score);
+//    if (score == 100)
+//    {
+//        printf("A\n");
+//        return 0;
+//    }
+//    if (score < 0 || score > 100)
+//    {
+//        printf("F\n");
+//        return 0;
+//    }
+//    switch ((score/10)%10)
+//    {
+//        case 9:
+//            printf("A\n");
+//            break;
+//        case 8:
+//            printf("B\n");
+//            break;
+//        case 7:
+//            printf("C\n");
+//            break;
+//        case 6:
+//            printf("D\n");
+//            break;
+//        default:
+//            printf("E\n");
+//            break;
+//    }
+//    return 0;
+//}
+//*/
+//
+//int main()
+//{
+//    int score = 0;
+//    scanf("%d", &score);
+//    if (score < 0 || score > 100)
+//    {
+//        printf("F\n");
+//    }
+//    else if (score >= 90)
+//    {
+//        printf("A\n");
+//    }
+//    else if (score >= 80)
+//    {
+//        printf("B\n");
+//    }
+//    else if (score >= 70)
+//    {
+//        printf("C\n");
+//    }
+//    else if (score >= 60)
+//    {
+//        printf("D\n");
+//    }
+//    else
+//    {
+//        printf("E\n");
+//    }
+//    return 0;
+//}
