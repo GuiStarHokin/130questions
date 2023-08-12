7eb6994ca87c1905383b006bd960acba3b33c481
hokin <jian.hee@qq.com>
2023-08-12 20:52:12 +0800

    commit

---
+++ 11-12.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int max3(int x, int y, int z)
+//{
+//    int max = x;
+//    max = max > y ? max : y;
+//    max = max > z ? max : z;
+//    return max;
+//}
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    scanf("%d %d %d", &a, &b, &c);
+//    double m = 0;
+//    m = ((double)max3(a + b, b, c)) / (max3(a, b + c, c) + max3(a, b, b + c));
+//    printf("%.2lf\n", m);
+//    return 0;
+//}
