9c87d108715cef76a970e172920a5bc2b552e961
hokin <jian.hee@qq.com>
2023-08-18 20:45:07 +0800

    commit

---
+++ 12-6.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+class TDate
+{
+private:
+    int Month;
+    int Day;
+    int Year;
+public:
+    TDate(int y = 0, int m = 0, int d = 0)
+    {
+        Year = y;
+        Month = m;
+        Day = d;
+    }
+    void print()
+    {
+        cout << Day << '/' << Month << '/' << Year << endl;
+    }
+};
+
+int main()
+{
+    int year = 0;
+    int month = 0;
+    int day = 0;
+    cin >> year >> month >> day;
+    TDate d(year, month, day);
+    d.print();
+    return 0;
+}
